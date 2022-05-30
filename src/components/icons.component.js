38beeb92e0ea7d03b953b3dd17c7f0d110321661
Shubhojeet Banerjee <shubhojeet@gmail.com>
2022-05-30 21:35:47 +0530

    Responsive 1

--- src/components/icons.component.js
+++ src/components/icons.component.js
@@ -3,20 +3,20 @@ function Icons() {
     return (
         <div className="px-2 py-2 md:px-12 md:py-12 items-center justify-center text-white">
             <h1 className="text-4xl">Global news, listings or partners</h1>
-            <div className="flex flex-row items-center justify-center mt-12">
-                <div className="mx-7">
+            <div className="flex flex-col md:flex-row items-center justify-center mt-12">
+                <div className="mx-7 my-5 md:my-auto">
                     <img src='BscScan.jpg' alt='BscScan Logo' height="100" width="100" />
                 </div>
-                <div className="mx-7">
+                <div className="mx-7 my-5 md:my-auto">
                     <img src='BlueHexa.png' alt='BscScan Logo' height="100" width="100" />
                 </div>
-                <div className="mx-7">
+                <div className="mx-7 my-5 md:my-auto">
                     <img src='CircleBunny.png' alt='BscScan Logo' height="100" width="100" />
                 </div>
-                <div className="mx-7">
+                <div className="mx-7 my-5 md:my-auto">
                     <img src='poop.png' alt='BscScan Logo' height="100" width="100" />
                 </div>
-                <div className="mx-7">
+                <div className="mx-7 my-5 md:my-auto">
                     <img src='YelloSquare.png' alt='BscScan Logo' height="100" width="100" />
                 </div>
             </div>
